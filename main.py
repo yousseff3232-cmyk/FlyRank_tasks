128d8aaf29d93060a2b8033dbf6a88c55b242e9e
youssef ahmed <yousseff3232@gmail.com>
2026-07-20 23:55:08 +0300

    Stage 1: root and health endpoints

--- main.py
+++ main.py
@@ -4,6 +4,13 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/hello")
-async def hello_server():
-    return {"message": "Hello from Fly Rank server!" }
+@app.get("/")
+async def get_api_root():
+    return {"name" : "Task API" ,
+            "version" : "1.0",
+            "endpoints" : ["/tasks"]}
+
+
+@app.get("/health")
+async def get_health():
+    return {"status" : "ok"}
