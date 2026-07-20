9ed679f64e2a3ef16d52dc20826d6179a6fe7abc
youssef ahmed <yousseff3232@gmail.com>
2026-07-21 00:21:04 +0300

    Stage 2: Read endpoints with 404

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI , HTTPException
 
 
 app = FastAPI()
 
+
+tasks_db =[
+
+    {"id":1 , "title": "go to the gym today" ,"done": False},
+    {"id":2 , "title": "Finish FlyRank task" , "done":True},
+    {"id":3 , "title": "Study MySql with BroCode Course " , "done": False}
+
+]
 
 @app.get("/")
 async def get_api_root():
@@ -14,3 +22,16 @@ async def get_api_root():
 @app.get("/health")
 async def get_health():
     return {"status" : "ok"}
+@app.get("/tasks")
+async def get_tasks():
+    return tasks_db
+
+
+@app.get("/tasks/{task_id}")
+async def get_single_task(task_id: int):
+
+  for task in tasks_db:
+      if task["id"] == task_id:
+          return task
+
+  raise HTTPException(status_code=404, detail="Task not found")
