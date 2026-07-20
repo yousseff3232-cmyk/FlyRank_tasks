9af17f46d552ae51029bf2473b3dc8b1659b2b9f
youssef ahmed <yousseff3232@gmail.com>
2026-07-21 01:02:51 +0300

    Stage 3: create with validation

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI , HTTPException
+from pydantic import BaseModel
+from typing import Optional
 
+from starlette import status
 
 app = FastAPI()
 
+
+class TaskCreate(BaseModel):
+  title: str
+  description: Optional[str] = None
 
 tasks_db =[
 
@@ -35,3 +42,23 @@ async def get_single_task(task_id: int):
           return task
 
   raise HTTPException(status_code=404, detail="Task not found")
+
+
+@app.post("/tasks" , status_code=status.HTTP_201_CREATED)
+async def create_new_task(task_payload: TaskCreate):
+
+    if not task_payload.title.strip():
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="VALIDATION ERROR :Title must not be empty or blank space")
+
+
+    new_id = max(task["id"] for task in tasks_db)+1 if tasks_db else 1
+    new_task= {
+        "id":new_id,
+        "title":task_payload.title.strip(),
+        "done":False
+
+}
+
+
+    tasks_db.append(new_task)
+    return new_task
