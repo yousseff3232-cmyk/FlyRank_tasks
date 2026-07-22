8e9e7844ff7fd63586b3a1f01f480ac70b9d467c
youssef ahmed <yousseff3232@gmail.com>
2026-07-22 04:13:29 +0300

    Stage 0: create SQLite database

--- main.py
+++ main.py
@@ -1,39 +1,81 @@
+import sqlite3
 from fastapi import FastAPI , HTTPException
-from pydantic import BaseModel
-from typing import Optional
-
+from pydantic import BaseModel ,Field
+from typing import List , Optional
 from starlette import status
 from starlette.responses import Response
 
-app = FastAPI()
+app = FastAPI(title= "Task API - SQLite Edition " , description= "A production-grade RESTFul API connected to SQLite" , version="2.0")
+
+
+DB_NAME = "tasks.db"
+
+
+
+
+def get_db_connection():
+
+    row_factory = sqlite3.Row
+
+    conn = sqlite3.connect(DB_NAME)
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+
+
+def init_db():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+
+    cursor.execute(""" CREATE TABLE IF NOT EXISTS tasks(id INTEGER PRIMARY KEY AUTOINCREMENT,title TEXT NOT NULL , done INTEGER DEFAULT 0 )""")
+
+
+    cursor.execute("SELECT COUNT(*) FROM tasks")
+    count = cursor.fetchone()[0]
+
+
+
+
+    if count == 0:
+        initial_task = [
+            ("Learning FastAPI fundamentals",1),
+            ("Build in-Memory CRUD API " ,1),
+            ("Connect CRUD API toSQLite Database",0)
+        ]
+        cursor.executemany("INSERT INTO tasks (title, done) VALUES (?,?)", initial_task)
+        conn.commit()
+    conn.close()
+
+@app.on_event("startup")
+def on_startup():
+    init_db()
+
+
 
 
 class TaskCreate(BaseModel):
-  title: str
-  description: Optional[str] = None
+  title: str = Field(...,min_length=1,
+  description = "Task title cannot be empty")
 
 class TaskUpdate(BaseModel):
-    title: str
-    done:Optional[bool]
+    title: str = Field(...,min_length=1, description="Updated task title")
+    done:bool
 
-tasks_db =[
-
-    {"id":1 , "title": "go to the gym today" ,"done": False},
-    {"id":2 , "title": "Finish FlyRank task" , "done":True},
-    {"id":3 , "title": "Study MySql with BroCode Course " , "done": False}
-
-]
-
-@app.get("/")
-async def get_api_root():
-    return {"name" : "Task API" ,
-            "version" : "1.0",
-            "endpoints" : ["/tasks"]}
+@app.get("/" , tags=["Metadata"])
+def get_api_root():
+    return {"name" : "SQlite To-Do API" ,
+            "DataBase" : "DB_NAME",
+            "status" : "active"}
 
 
-@app.get("/health")
-async def get_health():
-    return {"status" : "ok"}
+@app.get("/health" , tags=["Metadata"])
+def health_check():
+    return {"status" : "Healthy" , "database_connected": True }
+
+
+
+
 @app.get("/tasks")
 async def get_tasks():
     return tasks_db
